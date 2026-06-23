3772d8763ae1b300935905c9f4bff67c697c57c4
wangchao <wangchao@kangqiao.org.cn>
2026-06-23 15:38:54 +0800

    更改

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+Console.WriteLine("10.30");
+Console.WriteLine("10.30");
+Console.WriteLine("0623，15.34");
