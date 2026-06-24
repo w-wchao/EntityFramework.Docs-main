5372177b2947edc25a3c9f80a2efde046569ebb5
wangchao <wangchao@kangqiao.org.cn>
2026-06-24 10:15:53 +0800

    feat(test): 新增一行测试输出内容hello trae
    仅在测试文件中添加了一行简单的控制台打印测试语句，用于扩展测试输出样例

--- MyConsoleApp/test.cs
+++ MyConsoleApp/test.cs
@@ -7,5 +7,6 @@ partial class Program
         Console.WriteLine("hello hello world");
         Console.WriteLine("hello world123");
         Console.WriteLine("hello 456");
+        Console.WriteLine("hello trae");
     }
 }
